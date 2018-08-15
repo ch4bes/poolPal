dbea0e68b66df521c703984fc6470cfc9c52c7d1
James Lindsay <interior.two@gmail.com>
2018-08-15 15:49:06 -0700

    Clean up schedulerform.py
    Uses datetime library to handle time values. Time values are now created iteratively.

--- code/schedulerform.py
+++ code/schedulerform.py
@@ -1,21 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField, SubmitField
+import datetime
+
+hour_choices = []
+
+for i in range(24):
+    h = datetime.time(i)
+    hour_choices.append((h.strftime('%H'),h.strftime('%-I %p')))
 
 class SchedulerForm(FlaskForm):
-
     function = SelectField('Function', choices = [
         ('Light','Pool Light'),('Waterfall','Waterfall Pump'),('Main Pump','Main Pump')])
-
-    startTime = SelectField('Start Time', choices = [
-        ('00','12am'),('01','1am'),('02','2am'),('03','3am'),('04','4am'),('05','5am'),('06','6am'),
-        ('07','7am'),('08','8am'),('09','9am'),('10','10am'),('11','11am'),('12','12pm'),
-        ('13','1pm'),('14','2pm'),('15','3pm'),('16','4pm'),('17','5pm'),('18','6pm'),
-        ('19','7pm'),('20','8pm'),('21','9pm'),('22','10pm'),('23','11pm')])
-
-    stopTime = SelectField('Stop Time', choices = [
-        ('00','12am'),('01','1am'),('02','2am'),('03','3am'),('04','4am'),('05','5am'),('06','6am'),
-        ('07','7am'),('08','8am'),('09','9am'),('10','10am'),('11','11am'),('12','12pm'),
-        ('13','1pm'),('14','2pm'),('15','3pm'),('16','4pm'),('17','5pm'),('18','6pm'),
-        ('19','7pm'),('20','8pm'),('21','9pm'),('22','10pm'),('23','11pm')])
-
+    startTime = SelectField('Start Time', choices = [hour_choices])
+    stopTime = SelectField('Stop Time', choices = [hour_choices])
     submit = SubmitField("Send")
