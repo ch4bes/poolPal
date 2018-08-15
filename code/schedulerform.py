f08c3a2e272600c4b5a17372f11b16ac55d18922
James Lindsay <interior.two@gmail.com>
2018-08-15 15:57:03 -0700

    Add minutes to schedulerform.py
    Added minutes to schedulerform.py. Currently using quarter hour increments for minute values.

--- code/schedulerform.py
+++ code/schedulerform.py
@@ -3,14 +3,22 @@ from wtforms import SelectField, SubmitField
 import datetime
 
 hour_choices = []
+min_choices = []
 
 for i in range(24):
     h = datetime.time(i)
     hour_choices.append((h.strftime('%H'),h.strftime('%-I %p')))
+    
+for i in range(4):
+    n = i*15
+    m = datetime.time(0,n)
+    min_choices.append((str(i),m.strftime('%M')))
 
 class SchedulerForm(FlaskForm):
     function = SelectField('Function', choices = [
         ('Light','Pool Light'),('Waterfall','Waterfall Pump'),('Main Pump','Main Pump')])
-    startTime = SelectField('Start Time', choices = [hour_choices])
-    stopTime = SelectField('Stop Time', choices = [hour_choices])
+    startHour = SelectField('Hour', choices = [hour_choices])
+    startMin = SelectField('Minute', choices = [min_choices])
+    stopHour = SelectField('Hour', choices = [hour_choices])
+    stopMin = SelectField('Minute', choices = [min_choices])
     submit = SubmitField("Send")
