cd244aa4aec592a766d19e03275c71faada7a986
James Lindsay <interior.two@gmail.com>
2018-08-20 21:56:39 -0700

    Update schedulerform.py
    Import pins from conf.py, create function choices from pin names, create value and label variables for hour and minute choice tuples

--- code/schedulerform.py
+++ code/schedulerform.py
@@ -1,24 +1,36 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField, SubmitField
+from conf import pins
 import datetime
+
+function_choices = []
+
+for pin in pins:
+    choice = (pins[pin]['name'],pins[pin]['name'])
+    function_choices.append(choice)
 
 hour_choices = []
 min_choices = []
 
 for i in range(24):
     h = datetime.time(i)
-    hour_choices.append((h.strftime('%H'),h.strftime('%-I %p')))
-    
+    value = h.strftime('%H')
+    label = h.strftime('%-I %p')
+    choice = (value,label)
+    hour_choices.append(choice)
+
 for i in range(4):
     n = i*15
     m = datetime.time(0,n)
-    min_choices.append((str(i),m.strftime('%M')))
+    value = m.strftime('%M')
+    label = value
+    choice = (value, label)
+    min_choices.append(choice)
 
 class SchedulerForm(FlaskForm):
-    function = SelectField('Function', choices = [
-        ('Light','Pool Light'),('Waterfall','Waterfall Pump'),('Main Pump','Main Pump')])
-    startHour = SelectField('Hour', choices = [hour_choices])
-    startMin = SelectField('Minute', choices = [min_choices])
-    stopHour = SelectField('Hour', choices = [hour_choices])
-    stopMin = SelectField('Minute', choices = [min_choices])
+    function = SelectField('Function', choices = function_choices)
+    startHour = SelectField('Hour', choices = hour_choices)
+    startMin = SelectField('Minute', choices = min_choices)
+    stopHour = SelectField('Hour', choices = hour_choices)
+    stopMin = SelectField('Minute', choices = min_choices)
     submit = SubmitField("Send")
