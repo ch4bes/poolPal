cb9fa1320847ea3eaf3b4ed6ea94098dd2a59dfd
James Lindsay <interior.two@gmail.com>
2018-08-25 14:11:21 -0700

    Update conf.py
    Cleaned up useless code

--- code/conf.py
+++ code/conf.py
@@ -1,8 +1,6 @@
-version_prefix = 0
-version_suffix = 1
-version = version_prefix + version_suffix * .1
+version = '0.1'
 
-print('PoolPal - version ' + str(version))
+print('PoolPal - version ' + version)
 
 appDir = '/home/pi/poolPal/code/'
 dbFile = appDir + 'poolSchedules.db'
