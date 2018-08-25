9f03aa25901e97b2820bed2be0b37e533c39618f
James Lindsay <interior.two@gmail.com>
2018-08-25 16:18:35 -0700

    Update conf.py
    Add server_ip from main.py, rename server_ip to pool_pi_ip. Add esp_ip, add temp_db, rename dbFile to sched_db

--- code/conf.py
+++ code/conf.py
@@ -2,8 +2,12 @@ version = '0.1'
 
 print('PoolPal - version ' + version)
 
+pool_pi_ip = 127.0.0.1
+esp_ip = 10.0.3.183
+
 appDir = '/home/pi/poolPal/code/'
-dbFile = appDir + 'poolSchedules.db'
+sched_db = appDir + 'poolSchedules.db'
+temp_db = appDir + 'tempLog.db'
 
 pins = { # Pin Dictionary to store each pin number, name, and state
     17 : {'name':'Light', 'state':1},
