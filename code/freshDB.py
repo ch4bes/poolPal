2e725059318f65130ed03caafcb2d4536b8616f5
James Lindsay <interior.two@gmail.com>
2018-08-25 17:38:56 -0700

    Update freshDB.py
    Import sys to read dbFile from argv[1], create appropriate db file based on argv

--- code/freshDB.py
+++ code/freshDB.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-import os, sqlite3
-from conf import sched_db as dbFile
+import os, sqlite3, sys
+from conf import sched_db, temp_db
+
+dbFile = sys.argv[1]
 
 # delete former db, if it exists
 f = open(dbFile, 'w')
 f.close()
 
-# create fresh db
 conn = sqlite3.connect(dbFile)
-conn.execute('CREATE TABLE schedules (id INTEGER PRIMARY KEY, function TEXT, startHour INTEGER, startMin INTEGER, stopHour INTEGER, stopMin INTEGER)')
+
+if dbFile == sched_db:    
+    conn.execute('CREATE TABLE schedules (id INTEGER PRIMARY KEY, function TEXT, startHour INTEGER, startMin INTEGER, stopHour INTEGER, stopMin INTEGER)')
+if dbFile == temp_db:
+    conn.execute('CREATE TABLE temps (time INTEGER, temp FLOAT)')
+    
 conn.close()
